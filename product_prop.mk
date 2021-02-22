48bb54ca53305068aa990c52f4f6650438fc154c
Husker <misterhusker19@gmail.com>
2021-02-22 22:05:53 +0800

    jd2019: enable blur by default

--- product_prop.mk
+++ product_prop.mk
@@ -9,8 +9,10 @@ endif
 
 # Blurs
 PRODUCT_PRODUCT_PROPERTIES += \
-   ro.surface_flinger.supports_background_blur=1 \
-   persist.sys.sf.disable_blurs=1
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.sf.blurs_are_expensive=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1
 
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
